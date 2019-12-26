cea972b4317b055e65e3a5d07afce3c0fcff427d
makhatadze <vitomaxatadze@gmail.com>
2019-12-26 18:15:00 +0400

    Change calendar border and background colors

--- components/HolidaySettings.php
+++ components/HolidaySettings.php
@@ -13,14 +13,27 @@ use yii\base\Component;
 
 class HolidaySettings extends Component
 {
+    /**
+     * @var string manages holiday color by type on the calendar scheduler.
+     */
+    public $textColor = '#000000d6';
 
     /**
      * @var array manages holiday color by type on the calendar scheduler.
      */
-    public $calendarColor = [
+    public $calendarBackgroundColor = [
+        Holiday::TYPE_PERSONAL => '#08bbbb4d',
+        Holiday::TYPE_BUSINESS => '#ffd29b4d',
+        Holiday::TYPE_CUSTOM => '#ea7569'
+    ];
+
+    /**
+     * @var array manages holiday color by type on the calendar scheduler.
+     */
+    public $calendarBorderColor = [
         Holiday::TYPE_PERSONAL => '#63c598',
-        Holiday::TYPE_BUSINESS => '#ea7569',
-        Holiday::TYPE_CUSTOM => '#ecc262'
+        Holiday::TYPE_BUSINESS => '#ecc262',
+        Holiday::TYPE_CUSTOM => '#ea7569'
     ];
 
     public $customHolidayName = 'Custom';
@@ -29,8 +42,26 @@ class HolidaySettings extends Component
      * @param $type
      * @return mixed
      */
-    public function getColorByType($type)
+    public function getBackgroundColorByType($type)
     {
-        return $this->calendarColor[$type];
+        return $this->calendarBackgroundColor[$type];
+    }
+
+    /**
+     * @param $type
+     * @return mixed
+     */
+    public function getBorderColorByType($type)
+    {
+        return $this->calendarBorderColor[$type];
+    }
+
+    /**
+     * @param $type
+     * @return mixed
+     */
+    public function getTextColor()
+    {
+        return $this->textColor;
     }
 }
